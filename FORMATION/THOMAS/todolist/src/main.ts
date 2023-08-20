8887a37ae000d0a40c149fcfe3045d3cee7ad697
myriam <conceptmyriam@outlook.fr>
2023-08-20 23:03:08 +0200

    ok

--- FORMATION/THOMAS/todolist/src/main.ts
+++ FORMATION/THOMAS/todolist/src/main.ts
@@ -1,5 +1,3 @@
-import './style.css';
-
 // Récupérer les éléments du DOM
 const taskInput = document.getElementById('taskInput') as HTMLInputElement;
 const addButton = document.getElementById('addButton') as HTMLButtonElement;
@@ -17,8 +15,35 @@ function addTaskToList(taskText: string) {
   const li = document.createElement('li');
   li.textContent = taskText;
   
+  // Bouton de suppression
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Supprimer';
+  li.appendChild(deleteButton);
+  
   // Ajouter le nouvel élément li en haut de la liste des tâches
   taskList.prepend(li);
+
+  // Gestionnaire d'événements pour le clic sur le bouton de suppression
+  deleteButton.addEventListener('click', () => {
+    removeTaskFromList(li, taskText);
+  });
+}
+
+// Fonction de suppression
+function removeTaskFromList(li: HTMLLIElement, taskText: string) {
+  // Supprimer l'élément li de la liste visuelle
+  taskList.removeChild(li);
+  
+  // Récupérer les tâches existantes depuis le stockage local
+  const storedTasks = localStorage.getItem('tasks');
+  const tasks: string[] = storedTasks && typeof storedTasks === 'string' ? JSON.parse(storedTasks) : [];
+
+  // Supprimer la tâche du tableau des tâches
+  const taskIndex = tasks.indexOf(taskText);
+  if (taskIndex !== -1) {
+    tasks.splice(taskIndex, 1);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
 }
 
 // Enregistrer une nouvelle tâche dans le stockage local
